6541fded80964b120b03766a8944748e0372539a
Anders Lindmark <anders.lindmark@gmail.com>
2012-11-17 04:53:58 +0100

    More template related changes

--- shopping/views.py
+++ shopping/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hej")
+	#return HttpResponse("Hej")
+	return render_to_response("home.html")
 
 @login_required
 def loggedinonly(request):
